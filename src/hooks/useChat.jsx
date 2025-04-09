2ff1e8bcd71a0a6e8341362a36c3be2b0f043878
Leeveo <101552154+leeveo@users.noreply.github.com>
2025-04-09 17:55:18 +0200

    Update useChat.jsx

--- src/hooks/useChat.jsx
+++ src/hooks/useChat.jsx
@@ -4,7 +4,7 @@ import { createContext, useContext, useEffect, useState } from "react";
 const rawBackendUrl = import.meta.env.VITE_API_URL || (
   window.location.hostname === "localhost"
     ? "http://localhost:3000"
-    : "http://13.38.71.221:8080"
+    : "https://7d62-13-38-71-221.ngrok-free.app"
 );
 const backendUrl = rawBackendUrl.replace(/\/+$/, ""); // 🔧 Supprime les `/` finaux
 
